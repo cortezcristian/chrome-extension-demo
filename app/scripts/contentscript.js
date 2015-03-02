4094488019b2c9e48719796707295afeb09b4329
Cristian Cortez <cortez.cristian@gmail.com>
2015-03-02 01:44:31 -0300

    Unbind google events on search textbox

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -14,9 +14,25 @@ window.addEventListener('load', function(){
         console.log("Scoped")
     });
 
+    var input = document.getElementById('lst-ib');
+    input.setAttribute('ng-model', 'search');
+    //http://stackoverflow.com/questions/19469881/javascript-remove-all-event-listeners-of-specific-type
+    var inputClone = input.cloneNode(true);
+    input.parentNode.replaceChild(inputClone, input);
+    //angular.element(input).off();
+    angular.element(input).unbind();
+/*
+    angular.element(input)
+        .on('keydown change paste',function(e){
+            console.log(e)
+            //e.preventDefault();
+            e.stopPropagation();
+        });
+*/
     var myDirective = document.createElement('div');
     myDirective.setAttribute('my-directive', '');
     document.querySelector('.jsb center').appendChild(myDirective);
+    //document.querySelector('.sblsbb').appendChild(myDirective);
 
     app.directive('myDirective', function($sce){
         return {
