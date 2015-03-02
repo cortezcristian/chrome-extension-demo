51aac9bbbe03c2d11e23424b10ca8ad37258c228
Cristian Cortez <cortez.cristian@gmail.com>
2015-03-02 00:28:11 -0300

    Basic angular app

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,1 +1,12 @@
+'use strict';
 console.log("Up and running");
+
+window.addEventListener('load', function(){
+    var app = angular.module('Ducked', []);
+
+    var html = document.querySelector('html');
+    html.setAttribute('ng-app', '');
+    html.setAttribute('ng-csp', '');
+
+    angular.bootstrap(html, ['Ducked'], []);
+});
